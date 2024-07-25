ff5584d69b173b73166d873e10e84827dfb39443
Chandresh Sahay <chandreshsahay08874@gmail.com>
2024-07-25 11:31:05 +0530

    time compleity

---
+++ JavaProgram/src/BasicJava/naturalnumber.java
@@ -0,0 +1,26 @@
+package BasicJava;
+
+public class naturalnumber {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1; i<= 10; i++)
+            System.out.println(i);
+
+//        int i=10;
+//        while(i>=1) {
+//            System.out.println(i);
+//            i--;
+//    }
+//        int i=10;
+//        do{
+//            System.out.println(i);
+//            i--;
+//        }while(i>=1);
+
+    }
+
+
+}
+
+
+
---
+++ JavaProgram/src/BasicJava/printmyname.java
@@ -0,0 +1,8 @@
+package BasicJava;
+
+public class printmyname {
+    public static void main (String[] args){
+        System.out.println("Chandresh Sahay");
+    }
+}
+
